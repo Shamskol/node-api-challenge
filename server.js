0a0f6cbba529f95a8601f1c82794ebe473ce12f1
Shamskol <koladejun@yahoo.com>
2020-04-10 18:16:41 +0100

    Created the actions and projects folders

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const server = express();
+
+const projectsRouter = require("./projects/projectsRouter");
+const actionsRouter = require("./actions/actionsRouter");
+
+server.use(express.json());
+ server.use(logger);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+//custom middleware
+
+function logger(req, res, next) {
+  const { method, originalUrl } = req;
+  console.log(`${method} to ${originalUrl}`);
+
+  next();
+}
+
+server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectsRouter);
+
+module.exports = server;
