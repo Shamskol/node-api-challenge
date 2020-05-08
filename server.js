af7f3f25d30c1d0ab5756b04238e1d0e3816f8db
Shamskol <koladejun@yahoo.com>
2020-05-08 17:50:02 +0100

    Installed create-react-app

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const cors = require('cors');
 const server = express();
 
 const projectsRouter = require("./projects/projectsRouter");
 const actionsRouter = require("./actions/actionsRouter");
 
 server.use(express.json());
+server.use(cors())
  server.use(logger);
 
 server.get("/", (req, res) => {
